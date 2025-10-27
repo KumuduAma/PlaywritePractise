f8adcec70b71535695745d9b48b64704f6ab5c0b
KumuduAma <kumudu.amarasinghe@gmail.com>
2025-10-27 19:01:41 +0530

    added viewport size

--- tests/testGoogle.spec.ts
+++ tests/testGoogle.spec.ts
@@ -3,5 +3,7 @@ import{test, expect} from '@playwright/test'
 test('Open Google', async({page})=>{
   await page.goto("https://www.google.com/")
 
+  await page.setViewportSize({width:1290, height:1080})
+
   await page.waitForTimeout(2000)
 })
